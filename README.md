76fd033eaf8d9ce42a2be36088ada0859c8e5513
Hyunmin Kang <myhyuny@live.com>
2021-04-11 22:19:04 +0900

    Init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# heic
+# HEIC
+
+JPEG 용량이 너무 크고 내 iPhone, iPad, iCloud 용량은 너무 작고, 더 큰 용량을 구입하기엔 내 주머니가 너무 가볍다.  
+
+## 사용방법
+
+### Terminal
+
+```shell
+heic [images...]  
+```
+
+### Finder
+
+heic를 빌드해서 원하는 경로에 설치하고 automator 디렉토리의 안의 "Convert to HEIC.workflow"를 Automator 로 불러와 Run Shell Script 를 heic 설치한 경로에 맞게 수정해서 설치후 사용.  
+
+## 만드는데 도움을 주신 분들
+
+- Adobe
+- Phase One
+
+그 밖에 Apple 생태계 안에서 소프트웨어를 만드는데 HEIC 저정하기 및 내보내기를 지원해 주지 않는 모든 소프트웨어 업체들에게 깊은 ~~빡침~~ 감사를 드립니다.
---
+++ heic/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  heic
+//
+//  Created by Hyunmin Kang on 14/06/2019.
+//  Copyright © 2019 Hyunmin Kang. All rights reserved.
+//
+
+import AVFoundation
+
+let args = CommandLine.arguments;
+
+if args.count < 2 {
+	print("Usage: heic [images...]")
+	print()
+	exit(EXIT_SUCCESS)
+}
+
+let data = NSMutableData()
+let properties = [kCGImageDestinationLossyCompressionQuality: 0.65] as CFDictionary
+
+for file in args.dropFirst() {
+	autoreleasepool {
+        if FileManager.default.fileExists(atPath: file) == false {
+            print(file, "not found.")
+            exit(EXIT_FAILURE)
+        }
+        
+		let fromUrl = URL(fileURLWithPath: file)
+        
+		let toUrl = fromUrl.deletingPathExtension().appendingPathExtension("HEIC")
+		print(fromUrl.lastPathComponent + " -> " + toUrl.lastPathComponent)
+		
+		let source = CGImageSourceCreateWithURL(fromUrl as CFURL, nil)!
+        let destination = CGImageDestinationCreateWithData(data, AVFileType.heic as CFString, 1, nil)!
+        CGImageDestinationAddImageFromSource(destination, source, 0, properties)
+        CGImageDestinationFinalize(destination)
+        
+        data.write(to: toUrl, atomically: true)
+        data.resetBytes(in: NSRange(location: 0, length: data.length))
+        
+        try! FileManager.default.removeItem(at: fromUrl)
+	}
+}
