5cc420ac6cfd0681ec46703299560b92551b0d89
Hyunmin Kang <myhyuny@live.com>
2021-04-11 22:36:30 +0900

    Cleanup

--- heic/main.swift
+++ heic/main.swift
@@ -11,27 +11,27 @@ import AVFoundation
 let args = CommandLine.arguments;
 
 if args.count < 2 {
-	print("Usage: heic [images...]")
-	print()
-	exit(EXIT_SUCCESS)
+    print("Usage: heic [images...]")
+    print()
+    exit(EXIT_SUCCESS)
 }
 
 let data = NSMutableData()
 let properties = [kCGImageDestinationLossyCompressionQuality: 0.65] as CFDictionary
 
 for file in args.dropFirst() {
-	autoreleasepool {
+    autoreleasepool {
         if FileManager.default.fileExists(atPath: file) == false {
             print(file, "not found.")
             exit(EXIT_FAILURE)
         }
         
-		let fromUrl = URL(fileURLWithPath: file)
+        let fromUrl = URL(fileURLWithPath: file)
         
-		let toUrl = fromUrl.deletingPathExtension().appendingPathExtension("HEIC")
-		print(fromUrl.lastPathComponent + " -> " + toUrl.lastPathComponent)
-		
-		let source = CGImageSourceCreateWithURL(fromUrl as CFURL, nil)!
+        let toUrl = fromUrl.deletingPathExtension().appendingPathExtension("HEIC")
+        print(fromUrl.lastPathComponent + " -> " + toUrl.lastPathComponent)
+        
+        let source = CGImageSourceCreateWithURL(fromUrl as CFURL, nil)!
         let destination = CGImageDestinationCreateWithData(data, AVFileType.heic as CFString, 1, nil)!
         CGImageDestinationAddImageFromSource(destination, source, 0, properties)
         CGImageDestinationFinalize(destination)
@@ -40,5 +40,5 @@ for file in args.dropFirst() {
         data.resetBytes(in: NSRange(location: 0, length: data.length))
         
         try! FileManager.default.removeItem(at: fromUrl)
-	}
+    }
 }
