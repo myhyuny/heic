02e2ae00390a046f5cbcb7e97c9ac542a51bae25
Hyunmin Kang <myhyuny@live.com>
2024-09-20 03:05:52 +0900

    add raw support

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,42 +3,58 @@
 //  heic
 //
 //  Created by Hyunmin Kang on 14/06/2019.
-//  Copyright © 2019 Hyunmin Kang. All rights reserved.
 //
 
 import AVFoundation
+import ArgumentParser
+import CoreImage
 
-let args = CommandLine.arguments;
+@main
+struct Heic: ParsableCommand {
+    @Option(help: "Compress quality (0 ~ 100)")
+    var quality: Double?
 
-if args.count < 2 {
-    print("Usage: heic [images...]")
-    print()
-    exit(EXIT_SUCCESS)
-}
+    @Argument(help: "Input image files")
+    var files: [String]
 
-let data = NSMutableData()
-let properties = [kCGImageDestinationLossyCompressionQuality: 0.65] as CFDictionary
+    func run() throws {
+        for path in files {
+            let fromUrl = URL(fileURLWithPath: path)
+            let toUrl = fromUrl.deletingPathExtension().appendingPathExtension("HEIC")
 
-for file in args.dropFirst() {
-    autoreleasepool {
-        if FileManager.default.fileExists(atPath: file) == false {
-            print(file, "not found.")
-            exit(EXIT_FAILURE)
+            print(fromUrl.lastPathComponent, " -> ", toUrl.lastPathComponent)
+
+            let source = CGImageSourceCreateWithURL(fromUrl as CFURL, nil)!
+            let metadata =
+                CGImageSourceCopyPropertiesAtIndex(source, 0, nil)! as! [AnyHashable: Any]
+            // print("metadata:", metadata)
+
+            let properties: NSMutableDictionary = [:]
+            properties.addEntries(from: metadata)
+
+            let destination = CGImageDestinationCreateWithURL(
+                toUrl as CFURL, AVFileType.heic as CFString, 1, nil)!
+
+            switch fromUrl.pathExtension.uppercased() {
+            case "ARW", "CR2", "CR3", "NEF", "ORF", "PEF", "RAF", "RW2":
+                properties[kCGImageDestinationLossyCompressionQuality] = self.quality ?? 0.5
+
+                let colorSpace = CGColorSpace(name: CGColorSpace.displayP3_PQ)!
+                let fromImage = CIImage(contentsOf: fromUrl, options: [.expandToHDR: true])!
+                let toImage = CIContext().createCGImage(
+                    fromImage, from: fromImage.extent, format: .RGB10, colorSpace: colorSpace)!
+                CGImageDestinationAddImage(destination, toImage, properties as CFDictionary)
+
+            default:
+                let quality =
+                    self.quality
+                    ?? (metadata[kCGImagePropertyDepth] as? Int ?? 0 > 8 ? 0.5 : 0.65)
+                properties[kCGImageDestinationLossyCompressionQuality] = quality
+
+                CGImageDestinationAddImageFromSource(destination, source, 0, properties)
+            }
+
+            CGImageDestinationFinalize(destination)
         }
-        
-        let fromUrl = URL(fileURLWithPath: file)
-        
-        let toUrl = fromUrl.deletingPathExtension().appendingPathExtension("HEIC")
-        print(fromUrl.lastPathComponent + " -> " + toUrl.lastPathComponent)
-        
-        let source = CGImageSourceCreateWithURL(fromUrl as CFURL, nil)!
-        let destination = CGImageDestinationCreateWithData(data, AVFileType.heic as CFString, 1, nil)!
-        CGImageDestinationAddImageFromSource(destination, source, 0, properties)
-        CGImageDestinationFinalize(destination)
-        
-        data.write(to: toUrl, atomically: true)
-        data.resetBytes(in: NSRange(location: 0, length: data.length))
-        
-        try! FileManager.default.removeItem(at: fromUrl)
     }
 }
